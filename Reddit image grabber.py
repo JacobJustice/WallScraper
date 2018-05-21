fb3c6a05941682d2d9648afb85b5f1c7145ab80a
Jacob Justice <36245913+JacobJustice@users.noreply.github.com>
2018-05-21 03:26:11 -0400

    Update Reddit image grabber.py

--- Reddit image grabber.py
+++ Reddit image grabber.py
@@ -1,10 +1,11 @@
 import praw
 import urllib.request as req
+import datetime
 
 reddit = praw.Reddit(client_id='EefyTKRTsjD9Lg',
                      client_secret='sV8ASsVbBfj9zT5JTX8eegBb8sM',
                      username='prawspaceporn',
-                     password='spaceporn',  # this is not my actual password
+                     password='password',  # this is not my actual password
                      user_agent='prawspacepornbot')
 
 spaceporn = reddit.subreddit('spaceporn')
@@ -12,10 +13,10 @@ spaceporn = reddit.subreddit('spaceporn')
 # change time_filter to all, day, week etc. to change the parameters
 # change limit to however many images you want to download
 top_spaceporn = spaceporn.top(time_filter='day',
-                              limit=3)
+                              limit=5)
 
 for submission in top_spaceporn:
     if submission.preview['images'][0]['source']['width'] >= 1920 and submission.preview['images'][0]['source']['height'] >= 1080:
         req.urlretrieve(submission.preview['images'][0]['source']['url'],
                         # change 'space-images/' to the name of the directory you want to send the images
-                        "space-images/" + submission.title + ".jpg")
+                        "Z:\space-images\\" + submission.preview['images'][0]['id'] + " " + str(datetime.datetime.date(datetime.datetime.now())) + ".jpg")
